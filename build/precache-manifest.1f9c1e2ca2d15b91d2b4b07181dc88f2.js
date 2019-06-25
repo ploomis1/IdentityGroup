f81ab0a5ad77efc7e40a7355094dbd70982d4630
Parker Loomis <parker.j.loomis@wmich.edu>
2019-06-24 22:59:52 -0400

    created repo

---
+++ build/precache-manifest.1f9c1e2ca2d15b91d2b4b07181dc88f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f8112b4bf75f93e722550ca92f5460d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "79a6a6b6190638c26065",
+    "url": "/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "42b926e591f97240cb75",
+    "url": "/static/css/main.fd52820d.chunk.css"
+  },
+  {
+    "revision": "79a6a6b6190638c26065",
+    "url": "/static/js/2.d57d27e9.chunk.js"
+  },
+  {
+    "revision": "42b926e591f97240cb75",
+    "url": "/static/js/main.54fa390f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+class App extends Component {
+
+  logout = () => {
+    localStorage.removeItem('jwtToken');
+    window.location.reload();
+  }
+
+  render() {
+    return (
+      <div class="container">
+        <div class="panel panel-default">
+          <div class="panel-heading">
+            <h3 class="panel-title">
+              Logged In &nbsp;
+              {localStorage.getItem('jwtToken') &&
+                <button class="btn btn-primary" onClick={this.logout}>Logout</button>
+              }
+            </h3>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
